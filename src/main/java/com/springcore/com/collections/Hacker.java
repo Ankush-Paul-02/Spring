14210775937adfa368f81661d4ebffc9ddc5833d
Ankush <ankushpaulclg2002@gmail.com>
2023-07-30 00:13:20 +0530

    1st commit

---
+++ src/main/java/com/springcore/com/collections/Hacker.java
@@ -0,0 +1,12 @@
+package com.springcore.com.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Hacker {
+    private int id;
+    private List<String> devices;
+    private Set<String> addresses;
+    private Map<String, String> courses;
+}
