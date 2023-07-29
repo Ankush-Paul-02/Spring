add2759dd0aa7609389b76bd3a770c8f7f145d49
Ankush <ankushpaulclg2002@gmail.com>
2023-07-30 00:08:18 +0530

    Initial commit

---
+++ src/main/java/com/paul/Developer.java
@@ -0,0 +1,46 @@
+package com.paul;
+
+public class Developer {
+    private int id;
+    private String name;
+    private String skill;
+
+    public Developer() {
+        super();
+    }
+
+    public Developer(int id, String name, String skill) {
+        this.id = id;
+        this.name = name;
+        this.skill = skill;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" + "id=" + id + ", name='" + name + '\'' + ", skill='" + skill + '\'' + '}';
+    }
+}
