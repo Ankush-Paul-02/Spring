1205b044dbf4a9edb7976229cce98462e10759db
Ankush <ankushpaulclg2002@gmail.com>
2023-07-30 00:32:54 +0530

    2nd commit

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -1,12 +1,13 @@
 package com.springcore;
 
+import com.springcore.com.collections.Hacker;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
-        Developer developer = (Developer) context.getBean("developer");
-        System.out.println(developer);
+        Hacker hacker = (Hacker) context.getBean("hacker");
+        System.out.println(hacker);
     }
 }
--- src/main/java/com/springcore/com/collections/Hacker.java
+++ src/main/java/com/springcore/com/collections/Hacker.java
@@ -9,4 +9,57 @@ public class Hacker {
     private List<String> devices;
     private Set<String> addresses;
     private Map<String, String> courses;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<String> devices) {
+        this.devices = devices;
+    }
+
+    public Set<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<String> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Map<String, String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Map<String, String> courses) {
+        this.courses = courses;
+    }
+
+    public Hacker() {
+        super();
+    }
+
+    public Hacker(int id, List<String> devices, Set<String> addresses, Map<String, String> courses) {
+        this.id = id;
+        this.devices = devices;
+        this.addresses = addresses;
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Hacker{" +
+                "id=" + id +
+                ", devices=" + devices +
+                ", addresses=" + addresses +
+                ", courses=" + courses +
+                '}';
+    }
 }
